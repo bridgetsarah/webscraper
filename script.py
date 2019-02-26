b2de189b350eb99d2865b44108e8bee18f01f43e
Bridget Sarah <msbskelly@gmail.com>
2019-02-26 10:55:50 +0000

    Successfully scaped simple.html

--- script.py
+++ script.py
@@ -1,17 +1,7 @@
-import requests                                              #Standard library import
-from Beautifulsoup import BeautifulSoup                               #Third-party import - library
-soup = BeautifulSoup(html_doc, 'html.parser')
+from bs4 import BeautifulSoup
+import requests  
 
-print(soup.prettify())
+with open('simple.html') as html_file:                          #Open file object
+    soup = BeautifulSoup(html_file, 'lxml')                     #Html parser 'lxml'
 
-page_link ='http://devbk.me'
-
-#fetch the content from url
-
-page_response = requests.get(page_link, timeout=30)
-page_content = BeautifulSoup(page_response.content, "html.parser")
-
-entry content = page_content.find_all(class_='entry-content')
-
-#[<div class="main_price">Price: $66.68</div>,
-# <div class="main_price">Price: $56.68</div>]
+print(soup)    
